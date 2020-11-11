899bc2c8f0d9f4cc4b0b183acd9148954ce89411
IliaStepenko <stepenkoila@gmail.com>
2020-11-11 19:34:30 +0200

    fix plugins

--- bot/plugins/handlers.py
+++ bot/plugins/handlers.py
@@ -3,16 +3,23 @@ import datetime
 from pyrogram import filters, Client
 
 
+@Client.on_message(filters.command('check') & filters.me & filters.chat("me"))
+async def check(client, message):
+    await message.reply("Helloooooo")
+
+
 @Client.on_message(filters.command('tukan') )
 async def det_tukan(client, message):
     tukan_id = 303701321
     tukan = await client.get_users(tukan_id)
-    await message.reply(tukan['first_name'])
+    await message.reply('@'+tukan['username'])
 
 
 @Client.on_message(filters.command('dall') & filters.me & ~filters.chat("me"))
 async def delete_all_message(client, message):
     chat_id = message['chat']['id']
+
+    await message.delete()
 
     chat_messages = [message['message_id'] async for message in client.search_messages(chat_id=chat_id, from_user="me") ]
 
@@ -21,4 +28,5 @@ async def delete_all_message(client, message):
     result_string = f"Удаление ВСЕХ сообщений из чата {chat_id} в {datetime.datetime.now()}"
 
     await client.send_message("me", result_string)
+
 
--- bot/toolbot.py
+++ bot/toolbot.py
@@ -1,18 +1,23 @@
 from pyrogram import Client, filters
-
+import datetime
 from config import Config
+
+
+
+config = Config
 
 
 class ToolBot(Client):
 
     def __init__(self):
         super().__init__(
-            session_name=Config.SESSION,
-            api_id=Config.API_ID,
-            api_hash=Config.API_HASH,
-            plugins=dict(root="plugins")
+            session_name=config.SESSION,
+            api_id=config.API_ID,
+            api_hash=config.API_HASH,
+            plugins=dict(
+                root="plugins",
+                include=['handlers',]
+            )
         )
 
-        @self.on_message(filters.command('check') & filters.me & filters.chat("me"))
-        async def check(client, message):
-            await message.reply("Helloooooo")
+
