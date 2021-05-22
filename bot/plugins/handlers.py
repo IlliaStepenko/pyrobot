9e919d4006d8f08707e35fc1bd8b7b8f8c87042b
IliaStepenko <stepenkoila@gmail.com>
2021-05-22 18:27:34 +0300

    add self spam filter

--- bot/plugins/handlers.py
+++ bot/plugins/handlers.py
@@ -20,16 +20,29 @@ async def filter_channel(_, __, query):
 
 async def check_spam(message):
 
+    chat = getattr(message, 'chat')
+    if chat:
+        chat_id = chat.get('id')
+        chat_username = chat.get('username')
+
     message_text = getattr(message, 'text')
     if message_text:
-        if message_text.find('t.me/') != -1:
+        if message_text.find('t.me/') != -1 and message_text.find('@') != -1:
+
+            if message_text.find(chat_id) or message_text.find(chat_username):
+               return False
+
             if message_text.find('https://t.me/joinchat/AAAAAFCg99bpFf62A_f3yA') != -1:
                 return False
             return True
 
     caption = getattr(message, 'caption', None)
     if caption:
-        if caption.find('t.me/') != -1:
+        if caption.find('t.me/') != -1 and caption.find('@') != -1:
+
+            if caption.find(chat_id) or caption.find(chat_username):
+               return False
+
             if caption.find('https://t.me/joinchat/AAAAAFCg99bpFf62A_f3yA') != -1:
                 return False
             return True
@@ -43,8 +56,13 @@ async def check_spam(message):
             url = getattr(item, 'url', None)
             if url == 'https://t.me/joinchat/AAAAAFCg99bpFf62A_f3yA':
                 continue
-            elif url and url.find('t.me/') != -1:
+            elif url and url.find('t.me/') != -1 and url.find('@') != -1:
+
+                if url.find(chat_id) or url.find(chat_username):
+                    return False
+
                 return True
+
         return False
     else:
         return False
@@ -91,7 +109,7 @@ async def on_new_post(client, message):
 
 @Client.on_message(filters.command('lastn') & filters.me)
 async def get_new_post(client, message):
-    messages = await client.get_history(-1001495650439, limit=2)
-    print(messages[0])
+    messages = await client.get_history("insiderUKR", limit=2)
+    print(messages[1])
 
 
