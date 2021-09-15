c27a9a97b4c30f533761a47660d13cd692f1bd2f
IliaStepenko <stepenkoila@gmail.com>
2021-09-15 17:19:58 +0300

    change file buffer to io buffer

--- bot/plugins/personal.py
+++ bot/plugins/personal.py
@@ -1,7 +1,7 @@
 import datetime
 import os
+import io
 import sys
-
 from textblob import TextBlob
 from pyrogram import filters, Client
 
@@ -57,19 +57,11 @@ async def run_python(client, message):
 
     old_buffer = sys.stdout
     try:
-        with open('temp.txt', 'w') as output:
-            sys.stdout = output
-            output.write(f"code: \n{python_text}\n")
-            output.write("\nresult: \n")
-            exec(python_text)
-            sys.stdout = old_buffer
-
-        result = ""
-        with open('temp.txt', 'r') as result:
-            result = "".join(result.readlines())
-
-        os.remove('temp.txt')
-
+        sys.stdout = new_buffer = io.StringIO()
+        print(f"code: \n{python_text}\n")
+        print("\nresult\n")
+        exec(python_text)
+        sys.stdout = old_buffer
         await client.edit_message_text( message['chat']['id'], message['message_id'], result)
     except Exception as e:
         sys.stdout = old_buffer
