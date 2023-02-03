17c89e9b8070ac702ba6cdc7265684d6ae9fea02
IlliaStepenko <illia.stepenko@softformance.com>
2023-02-03 16:43:47 +0200

    Update main.py
    s

--- bot/database/main.py
+++ bot/database/main.py
@@ -40,30 +40,5 @@ class AsyncDataSource:
 
     async def close(self):
         pass
-        # self.conn.close()
-        # self.cursor.close()
+       
 
-
-class DataSource:
-
-    def __init__(self):
-        try:
-            from .local_config import DBConfig as Config
-            self.conn = psycopg2.connect(**Config.as_conn_param_dict())
-        except Exception as e:
-            database_url = os.environ['DATABASE_URL']
-            self.conn = psycopg2.connect(database_url, sslmode='require')
-
-        self.cursor = self.conn.cursor()
-
-    def get_target_chats(self):
-        self.cursor.execute('''SELECT * FROM public."TargetChats";''')
-        return self.cursor.fetchall()
-
-    def get_source_chats(self):
-        self.cursor.execute('''SELECT * FROM public."SourceChats";''')
-        return self.cursor.fetchall()
-
-    def close(self):
-        self.conn.close()
-        self.cursor.close()
