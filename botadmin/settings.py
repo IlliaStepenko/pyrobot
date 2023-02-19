5a05bda5ce6178b2d88deeb3b21e80a6e1de03e5
iliast <stepenkoila@gmail.com>
2023-02-19 20:08:47 +0200

    add to allowed hosts

--- botadmin/settings.py
+++ botadmin/settings.py
@@ -41,7 +41,7 @@ except ImportError:
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://toolsbotins.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['toolsbotins.herokuapp.com', '127.0.0.1']
 
 # Application definition
 
