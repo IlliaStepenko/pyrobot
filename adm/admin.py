f40ee19362631e034e411a4ca4e00827ec2892e9
iliast <stepenkoila@gmail.com>
2023-02-19 20:57:11 +0200

    add to admin

--- adm/admin.py
+++ adm/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from adm.models import Sourcechats, Targetchats, Whitelist
+
 # Register your models here.
+
+admin.site.register(Sourcechats)
+admin.site.register(Targetchats)
+admin.site.register(Whitelist)
