304cc0b2625755d7576b8b53310c6831440b8b42
iliast <stepenkoila@gmail.com>
2023-02-25 23:10:23 +0200

    imgkit

--- bot/__main__.py
+++ bot/__main__.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+import subprocess
 
 import openai
 from pyrogram.methods.utilities.idle import idle
@@ -8,7 +9,6 @@ from toolbot import ToolBot
 
 
 async def main(bot=None):
-
     try:
         from local_config import LocalConfig
         openai.api_key = LocalConfig.OPEN_AI_KEY
@@ -20,8 +20,18 @@ async def main(bot=None):
     await idle()
     await bot.stop()
 
+
 if __name__ == "__main__":
+    if 'DYNO' in os.environ:
+        print('loading wkhtmltopdf path on heroku')
+        WKHTMLTOPDF_CMD = subprocess.Popen(
+            ['which', os.environ.get('WKHTMLTOPDF_BINARY', 'wkhtmltopdf-pack')],
+            # Note we default to 'wkhtmltopdf' as the binary name
+            stdout=subprocess.PIPE).communicate()[0].strip()
+    else:
+        print('loading wkhtmltopdf path on localhost')
+        MYDIR = os.path.dirname(__file__)
+        WKHTMLTOPDF_CMD = os.path.join(MYDIR + "/static/executables/bin/", "wkhtmltopdf.exe")
     bot = ToolBot()
     loop = asyncio.get_event_loop()
     loop.run_until_complete(main(bot))
-
