450ffda9730dbdf2e9504c47ba7c0c95088acb8f
Illia Stepenko <i.stepenko@sdh-it.de>
2024-05-05 01:29:27 +0300

    restore news

--- bot/plugins/handlers.py
+++ bot/plugins/handlers.py
@@ -64,103 +64,97 @@ async def recalculate_target_source(client, message):
     client.target_chats = [chat[1] for chat in await client.data_source.get_target_chats() if chat[3]]
     client.whitelist = [item[1].strip() for item in await client.data_source.get_white_list()]
 
-#
-# @Client.on_message(filters.channel & filters.create(filter_channel))
-# async def on_new_post(client, message):
-#     num = random.random() * 10
-#
-#     if num % 3 == 0:
-#         for chat_id in client.target_chats:
-#             await client.send_message(chat_id, "Error: could not find source chat.")
-#     else:
-#         chat = getattr(message, 'chat')
-#         if chat:
-#             chat_id = str(getattr(chat, 'id', None))
-#             chat_username = str(getattr(chat, 'username', None))
-#         else:
-#             return None
-#
-#         media_group_id = getattr(message, 'media_group_id', None)
-#         message_items = set()
-#
-#         if media_group_id:
-#             if media_group_id != client.last_media_group:
-#                 client.last_media_group = media_group_id
-#                 media_group_to_send = []
-#                 media_group_messages = await client.get_media_group(chat_id, message.id)
-#
-#                 for message in media_group_messages:
-#
-#                     for item, media_class in media_types.items():
-#                         media_obj = getattr(message, item, None)
-#                         caption = getattr(message, 'caption', None)
-#                         if caption is not None:
-#                             message_items.add(caption)
-#
-#                         if caption and await check_spam(chat_id, chat_username, caption, client.whitelist):
-#                             return None
-#
-#                         entities = getattr(message, 'entities', [])
-#
-#                         if not entities:
-#                             entities = getattr(message, 'caption_entities', [])
-#
-#                         if entities:
-#                             for entity in entities:
-#                                 url = getattr(entity, 'url', None)
-#
-#                                 if url and await check_spam(chat_id, chat_username, url, client.whitelist):
-#                                     await client.send_message("me", f"block spam in url {url}")  # remove
-#                                     return None
-#
-#                         if media_obj:
-#                             media_group_to_send.append(
-#                                 media_class(media_obj.file_id, caption=caption)
-#                             )
-#                 if await check_duplicate(chat_id, message_items):
-#                     await client.send_message("me", f"block duplicate in message_items {str(message_items)}")  # remove
-#                     return None
-#
-#                 for chat_id in client.target_chats:
-#                     await client.send_media_group(chat_id, media_group_to_send)
-#         else:
-#             message_text = getattr(message, 'text', None)
-#
-#             caption = getattr(message, 'caption', None)
-#
-#             if message_text is not None:
-#                 message_items.add(message_text)
-#
-#             if caption is not None:
-#                 message_items.add(caption)
-#
-#             caption_entities = getattr(message, 'caption_entities', None)
-#             if caption_entities is not None:
-#                 for item in caption_entities:
-#                     caption_url = getattr(item, 'url', None)
-#                     if caption_url and await check_spam(chat_id, chat_username, caption_url, client.whitelist):
-#                         await client.send_message("me", f"block spam in caption_entity {caption_url}")  # remove
-#                         return None
-#
-#             message_entities = getattr(message, 'entities', None)
-#             if message_entities is not None:
-#                 for item in message_entities:
-#                     message_url = getattr(item, 'url', None)
-#                     if message_url and await check_spam(chat_id, chat_username, message_url, client.whitelist):
-#                         await client.send_message("me", f"block spam in message entity {message_url}")  # remove
-#                         return None
-#
-#             if message_text is not None and await check_spam(chat_id, chat_username, message_text, client.whitelist):
-#                 await client.send_message("me", f"block spam in message_text {message_text}")  # remove
-#                 return None
-#
-#             if caption is not None and await check_spam(chat_id, chat_username, caption, client.whitelist):
-#                 await client.send_message("me", f"block spam in caption {caption}")  # remove
-#                 return None
-#
-#             if await check_duplicate(chat_id, message_items):
-#                 await client.send_message("me", f"block duplicate in message_text {message_text}")  # remove
-#                 return None
-#
-#             for chat_id in client.target_chats:
-#                 await client.copy_message(chat_id, message.chat.id, message.id)
+
+@Client.on_message(filters.channel & filters.create(filter_channel))
+async def on_new_post(client, message):
+    chat = getattr(message, 'chat')
+    if chat:
+        chat_id = str(getattr(chat, 'id', None))
+        chat_username = str(getattr(chat, 'username', None))
+    else:
+        return None
+
+    media_group_id = getattr(message, 'media_group_id', None)
+    message_items = set()
+
+    if media_group_id:
+        if media_group_id != client.last_media_group:
+            client.last_media_group = media_group_id
+            media_group_to_send = []
+            media_group_messages = await client.get_media_group(chat_id, message.id)
+
+            for message in media_group_messages:
+
+                for item, media_class in media_types.items():
+                    media_obj = getattr(message, item, None)
+                    caption = getattr(message, 'caption', None)
+                    if caption is not None:
+                        message_items.add(caption)
+
+                    if caption and await check_spam(chat_id, chat_username, caption, client.whitelist):
+                        return None
+
+                    entities = getattr(message, 'entities', [])
+
+                    if not entities:
+                        entities = getattr(message, 'caption_entities', [])
+
+                    if entities:
+                        for entity in entities:
+                            url = getattr(entity, 'url', None)
+
+                            if url and await check_spam(chat_id, chat_username, url, client.whitelist):
+                                await client.send_message("me", f"block spam in url {url}")  # remove
+                                return None
+
+                    if media_obj:
+                        media_group_to_send.append(
+                            media_class(media_obj.file_id, caption=caption)
+                        )
+            if await check_duplicate(chat_id, message_items):
+                await client.send_message("me", f"block duplicate in message_items {str(message_items)}")  # remove
+                return None
+
+            for chat_id in client.target_chats:
+                await client.send_media_group(chat_id, media_group_to_send)
+    else:
+        message_text = getattr(message, 'text', None)
+
+        caption = getattr(message, 'caption', None)
+
+        if message_text is not None:
+            message_items.add(message_text)
+
+        if caption is not None:
+            message_items.add(caption)
+
+        caption_entities = getattr(message, 'caption_entities', None)
+        if caption_entities is not None:
+            for item in caption_entities:
+                caption_url = getattr(item, 'url', None)
+                if caption_url and await check_spam(chat_id, chat_username, caption_url, client.whitelist):
+                    await client.send_message("me", f"block spam in caption_entity {caption_url}")  # remove
+                    return None
+
+        message_entities = getattr(message, 'entities', None)
+        if message_entities is not None:
+            for item in message_entities:
+                message_url = getattr(item, 'url', None)
+                if message_url and await check_spam(chat_id, chat_username, message_url, client.whitelist):
+                    await client.send_message("me", f"block spam in message entity {message_url}")  # remove
+                    return None
+
+        if message_text is not None and await check_spam(chat_id, chat_username, message_text, client.whitelist):
+            await client.send_message("me", f"block spam in message_text {message_text}")  # remove
+            return None
+
+        if caption is not None and await check_spam(chat_id, chat_username, caption, client.whitelist):
+            await client.send_message("me", f"block spam in caption {caption}")  # remove
+            return None
+
+        if await check_duplicate(chat_id, message_items):
+            await client.send_message("me", f"block duplicate in message_text {message_text}")  # remove
+            return None
+
+        for chat_id in client.target_chats:
+            await client.copy_message(chat_id, message.chat.id, message.id)
